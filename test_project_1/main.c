e25ffb966bab049d4f13a88da7107c385168b384
frantic333 <frantic33@yandex.ru>
2025-06-04 01:24:29 +0300

    Initial Commit

---
+++ test_project_1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test_project_1
+//
+//  Created by Максим Шатурный on 04.06.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
